a7e1b4759af2a12094a4c7bba78f0b3366396401
spamprx <cs23btech11012@iith.ac.in>
2024-07-15 18:53:04 +0530

    Uploded the Navbar section and Hero section of the webpage into src

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar";
+import Highlights from "./components/Highlights";
+import Hero from "./components/Hero";
+
+
+
+const App = () =>{
+  return (
+    <main className="bg-black">
+    <Navbar />
+    <Hero />
+    <Highlights />
+    </main>
+  )
+}
+export default App
